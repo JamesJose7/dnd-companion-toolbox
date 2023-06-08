e833838324be18ce125dff3319b8d0c3a4f8dcca
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2023-06-07 23:00:06 -0700

    Add currency distribution when division has remainder

--- src/utils/calculator/currencyShareCalculator.js
+++ src/utils/calculator/currencyShareCalculator.js
@@ -59,7 +59,13 @@ const calculateCurrencyShareDistribution = (players, currencies) => {
       const currencyResult = buildEvenResult(currency, players, basePay);
       results.push(currencyResult);
     } else {
-      results.push(singleResultMock);
+      const currencyResult = buildResultWithRemainder(
+        currency,
+        players,
+        basePay,
+        currencyRemainder
+      );
+      results.push(currencyResult);
     }
   });
 
@@ -73,9 +79,54 @@ const buildEvenResult = (currency, players, basePay) => {
     currencyShare: basePay,
   }));
   return {
-    currency: currency,
-    distribution: distribution,
+    currency,
+    distribution,
   };
+};
+
+const buildResultWithRemainder = (
+  currency,
+  players,
+  basePay,
+  currencyRemainder
+) => {
+  let distribution = [];
+  // Roll D20 for each player until there are no ties
+  // This gives the positioning to receive the remainder
+  let isRollTied = false;
+  do {
+    isRollTied = false;
+    const rolls = [];
+    distribution = [];
+    for (let i = 0; i < players.length; i++) {
+      const player = players[i];
+      const d20Roll = rollNDice(20);
+      if (rolls.includes(d20Roll)) {
+        isRollTied = true;
+      } else {
+        rolls.push(d20Roll);
+      }
+
+      distribution.push({ player, d20Roll, currencyShare: basePay });
+    }
+  } while (isRollTied);
+
+  // Sort players based on d20 roll
+  distribution.sort((a, b) => b.d20Roll - a.d20Roll);
+
+  // Add an additional currency count to players based on the remainder
+  for (let remainder = 0; remainder < currencyRemainder; remainder++) {
+    distribution[remainder].currencyShare++;
+  }
+
+  return {
+    currency,
+    distribution,
+  };
+};
+
+const rollNDice = (n) => {
+  return Math.floor(Math.random() * n) + 1;
 };
 
 export default calculateCurrencyShareDistribution;
